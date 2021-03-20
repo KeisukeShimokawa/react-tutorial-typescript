ee5cf6f14f7470ef294d1704b9ab0ffa25e8faaa
KeisukeShimokawa <shimoke4869@gmail.com>
2021-03-21 01:38:44 +0900

    feta: ゲームの勝利までの操作をE2Eテストに追加

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -1,15 +1,45 @@
-describe('My First Test', () => {
-  it('Does not do much!', () => {
-    // cy.visit('http://localhost:3000');
-    // cy.visit("/")
-    cy.visit('https://example.cypress.io');
+describe('TicTacToe Integration Test', () => {
+  beforeEach(() => {
+    // 初期画面をロードする
+    cy.visit('/');
+    // 初期Squareが全て空文字であること
+    cy.get('[data-e2e=square]').each(($el) => {
+      expect($el).to.contain('');
+    });
+    // 初期StatusにプレイヤーXが表示されていること
+    cy.get('[data-e2e=status]').should('have.text', 'Next Player: X');
+    // 初期Moveの配列長が1であること
+    cy.get('[data-e2e=move]').should('have.length', 1);
+  });
 
-    cy.contains('type').click();
+  it('プレイヤーXが5手目で勝利する', () => {
+    /**
+     * 以下の状態でプレイヤーXが勝利する
+     *
+     * X(3) _    _
+     * O(4) X(1) _
+     * O(2) _    X(5)
+     */
 
-    cy.url().should('include', '/commands/actions');
-
-    cy.get('.action-email')
-      .type('fake@email.com')
-      .should('have.value', 'fake@email.com');
+    // 1手目
+    cy.get('[data-e2e=square]').eq(4).click().should('have.text', 'X');
+    cy.get('[data-e2e=status]').should('have.text', 'Next Player: O');
+    cy.get('[data-e2e=move]').eq(1).should('have.text', 'Go to move #1');
+    // 2手目
+    cy.get('[data-e2e=square]').eq(6).click().should('have.text', 'O');
+    cy.get('[data-e2e=status]').should('have.text', 'Next Player: X');
+    cy.get('[data-e2e=move]').eq(2).should('have.text', 'Go to move #2');
+    // 3手目
+    cy.get('[data-e2e=square]').eq(0).click().should('have.text', 'X');
+    cy.get('[data-e2e=status]').should('have.text', 'Next Player: O');
+    cy.get('[data-e2e=move]').eq(3).should('have.text', 'Go to move #3');
+    // 4手目
+    cy.get('[data-e2e=square]').eq(3).click().should('have.text', 'O');
+    cy.get('[data-e2e=status]').should('have.text', 'Next Player: X');
+    cy.get('[data-e2e=move]').eq(4).should('have.text', 'Go to move #4');
+    // 5手目
+    cy.get('[data-e2e=square]').eq(8).click().should('have.text', 'X');
+    cy.get('[data-e2e=status]').should('have.text', 'Winner: X');
+    cy.get('[data-e2e=move]').eq(5).should('have.text', 'Go to move #5');
   });
 });
